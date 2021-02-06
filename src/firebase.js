0fea4ab3de6293df268ce64bfc79c6d47a59ed1e
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-02-06 20:36:28 +0900

    Initializing

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCd7R2aV1IWwTOeZlBVyHav93PDN81fitc",
+  authDomain: "nwitter-9aea2.firebaseapp.com",
+  projectId: "nwitter-9aea2",
+  storageBucket: "nwitter-9aea2.appspot.com",
+  messagingSenderId: "821419518748",
+  appId: "1:821419518748:web:216e6073b0e784952028d9",
+};
+
+export default firebase.initializeApp(firebaseConfig);
