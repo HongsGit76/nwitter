a2bf590f3a5f770667672a8348c91df6c8f2b295
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-02-06 21:28:31 +0900

    settings routes

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCd7R2aV1IWwTOeZlBVyHav93PDN81fitc",
-  authDomain: "nwitter-9aea2.firebaseapp.com",
-  projectId: "nwitter-9aea2",
-  storageBucket: "nwitter-9aea2.appspot.com",
-  messagingSenderId: "821419518748",
-  appId: "1:821419518748:web:216e6073b0e784952028d9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
