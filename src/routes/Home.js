b7632f3edd9292d685c00b8a78e63c59b21d5ac0
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-02-19 00:18:54 +0900

    get database contents => collects

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,16 @@
 import { dbService } from "fbase";
-import react, { useState } from "react";
+import react, { useEffect, useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => console.log(document.data()));
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("nweets").add({
